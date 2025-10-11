942d2f6760898e3714ae6a7ab7dcf6914cdb2533
Alexandros Tzamalis <alextzamalis16@gmail.com>
2025-10-11 11:24:29 +0300

    MainClass code deletion to branch it out of the main class

--- src/me/alextzamalis/MainClass.java
+++ src/me/alextzamalis/MainClass.java
@@ -14,41 +14,13 @@ public class MainClass {
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
         UUID uuid = new UUID(random.nextLong(), random.nextLong());
-        System.out.println(uuid);
 
         User user = new User();
 
         Date curretDate = new Date();
         System.out.println(curretDate);
 
-        //variables
-        String userName;
-        int userAge;
-        String userPassword; // convert to hash!!!
-
-        System.out.println("WELCOME TO RANDOM GAME!");
-        System.out.println("Before starting we need your personal information and your consent!");
-        System.out.println();
-        System.out.print("Name: ");
-        userName = scan.nextLine();
-
-        System.out.print("Age: ");
-        userAge = scan.nextByte();
-        while (userAge < 21) {
-            System.out.print("Name above 21: ");
-            userAge = scan.nextByte();
-        }
-
-        scan.nextLine(); // clears nextline
-        System.out.print("Password: ");
-        userPassword = scan.nextLine();
-        while(userPassword.length() < 8) {
-            System.out.print("Password must be 8 digits lond and above: ");
-            userPassword = scan.nextLine();
-        }
-
-        generateRandomPull(uuid);
-
+        //generateRandomPull(uuid);
 
     }
 
