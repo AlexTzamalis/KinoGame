2042ee10635ce2059a9f0d2b1ed8e9945ac1588d
Alexandros Tzamalis <alextzamalis16@gmail.com>
2025-10-11 11:44:25 +0300

    qol fix

--- src/me/alextzamalis/MainClass.java
+++ src/me/alextzamalis/MainClass.java
@@ -3,7 +3,6 @@ package me.alextzamalis;
 import me.alextzamalis.process.User;
 import me.alextzamalis.util.MessageUtil;
 
-import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.UUID;
@@ -11,18 +10,13 @@ import java.util.UUID;
 public class MainClass {
     public static void main(String[] args) {
 
-        Random random = new Random();
-        Scanner scan = new Scanner(System.in);
-        UUID uuid = new UUID(random.nextLong(), random.nextLong());
+        Random random = new Random();                                   // Generates a random number
+        Scanner scan = new Scanner(System.in);                          // User input (might remove it from the main class
+        UUID uuid = new UUID(random.nextLong(), random.nextLong());     // Generates random UUID with Long numbers
+        MessageUtil messageUtil = new MessageUtil();                    // Calls few starting messages in app startup
+        User user = new User();                                         // starts off for user sign up/sign in
 
-        MessageUtil messageUtil = new MessageUtil();
-        User user = new User();
 
-
-    }
-
-    public static void generateRandomPull(UUID uuid) {
-        System.out.println(uuid);
 
     }
 
