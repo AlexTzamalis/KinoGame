cd0ba79e6439bef850e39ebc79e3370ad5788467
Alexandros Tzamalis <alextzamalis16@gmail.com>
2025-10-16 22:17:33 +0300

    New User manager. made to remove the UserInputs

---
+++ src/me/alextzamalis/login/UserManager.java
@@ -0,0 +1,10 @@
+package me.alextzamalis.login;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UserManager {
+    private List<User> users = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+}
