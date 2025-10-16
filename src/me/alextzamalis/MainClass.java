a91294e0f06ee63e2f1956f626eaf38bda2218e3
Alexandros Tzamalis <alextzamalis16@gmail.com>
2025-10-16 22:40:29 +0300

    commented all code calls to make room for the new User managers

--- src/me/alextzamalis/MainClass.java
+++ src/me/alextzamalis/MainClass.java
@@ -1,6 +1,7 @@
 package me.alextzamalis;
 
 import me.alextzamalis.file.FileWriters;
+import me.alextzamalis.login.UserManager;
 import me.alextzamalis.process.UserProcess;
 import me.alextzamalis.scanner.UserInput;
 import me.alextzamalis.util.MessageUtil;
@@ -20,37 +21,39 @@ public class MainClass {
         FileWriters fileWriters = new FileWriters();                                        // Writes text to files
         UserProcess userProcess = new UserProcess(userInput, messageUtil, fileWriters);     // starts off for user sign up/sign in
 
-        userInput.setUserProcess(userProcess);
-        messageUtil.setUserInput(userInput);
-        messageUtil.setUserProcess(userProcess);
-
-        // APPLICATION START
-        messageUtil.currentDate();
-        messageUtil.welcomeMesasge();
-        messageUtil.signInsignUpMessage();
-
-        // SIGN IN
-        userInput.userSignInSignUp();
-
-        if (userInput.signInSignUpCheck()) {
-            userInput.userEmailInput(messageUtil);
-            userInput.userPasswordInput(messageUtil);
-
-            messageUtil.successfulSignIn(userProcess.getUserFirstName());
-        }
-        // SIGN UP
-        else{
-            userInput.userFirstNameInput(messageUtil);
-            userInput.userLastNameInput(messageUtil);
-            userInput.userAgeInput(messageUtil);
-            userInput.userEmailInput(messageUtil);
-            userInput.userPasswordInput(messageUtil);
-            userProcess.collectUserCredentials();
-
-            messageUtil.successfulSignUp();
-        }
-
-        // application termination
-        userInput.closeScanner();
+        UserManager manager = new UserManager();
+        manager.start();
+//        userInput.setUserProcess(userProcess);
+//        messageUtil.setUserInput(userInput);
+//        messageUtil.setUserProcess(userProcess);
+//
+//        // APPLICATION START
+//        messageUtil.currentDate();
+//        messageUtil.welcomeMesasge();
+//        messageUtil.signInsignUpMessage();
+//
+//        // SIGN IN
+//        userInput.userSignInSignUp();
+//
+//        if (userInput.signInSignUpCheck()) {
+//            userInput.userEmailInput(messageUtil);
+//            userInput.userPasswordInput(messageUtil);
+//
+//            messageUtil.successfulSignIn(userProcess.getUserFirstName());
+//        }
+//        // SIGN UP
+//        else{
+//            userInput.userFirstNameInput(messageUtil);
+//            userInput.userLastNameInput(messageUtil);
+//            userInput.userAgeInput(messageUtil);
+//            userInput.userEmailInput(messageUtil);
+//            userInput.userPasswordInput(messageUtil);
+//            userProcess.collectUserCredentials();
+//
+//            messageUtil.successfulSignUp();
+//        }
+//
+//        // application termination
+//        userInput.closeScanner();
     }
 }
